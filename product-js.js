7186071b62c31d2b1c10797835c05868abe63929
malagorzata <gosia.warwaszynska@gmail.com>
2021-04-29 19:00:28 +0200

    adding JS to product view

---
+++ product-js.js
@@ -0,0 +1,35 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("_id");
+const url = "https://kea2021-907c.restdb.io/rest/bags/60816c663e28515100031b63";
+// const mediaurl = "https://kea2021-907c.restdb.io/media/";
+
+/*API key*/
+const options = {
+  headers: {
+    "x-apikey": "602e264f5ad3610fb5bb6267",
+  },
+};
+
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+
+  .then((data) => {
+    handleBags(data);
+  })
+  .catch((e) => {
+    console.error("an error occured:", e.message);
+  });
+
+function handleBags(bags) {
+  console.log(bags);
+  document.querySelector("h1.nameBag").textContent = bags.name;
+  document.querySelector("h1.priceBag").textContent = bags.price;
+  document.querySelector(".list li").textContent = bags.material;
+
+  document.querySelector(".bagDesc").textContent = bags.description;
+}
