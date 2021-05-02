39f09a676946f0280b86f74d202d12a1a492275a
Anna Cybulska <anna357y@stud.kea.dk>
2021-05-02 22:38:05 +0200

    JS-a lot new-Types&FiltersOnClickAreSeenAsSelected

--- all_products_site/all_products_site.js
+++ all_products_site/all_products_site.js
@@ -1,7 +1,47 @@
+const searchParams = new URLSearchParams(window.location.search);
+const articleID = searchParams.get("name");
+
 // TO DO
 // event listeners on types of bags, when selected adding class to "a" - ".selected_type", and removing it from previously selected type of bag
 
-console.log(document.querySelector("div#filter-type_of_bag a"));
+let container_types_of_bags = document.getElementById("filter-type_of_bag");
+console.log(container_types_of_bags);
+// console.log(container_types_of_bags.childNodes);
+console.log(container_types_of_bags.children);
+
+let bag_types = [];
+
+container_types_of_bags.forEach((type) => {
+    let types = document.querySelector("#filter-type_of_bag").children;
+    console.log(types);
+});
+
+
+type.addEventListener("click", selected_type);
+
+
+function selected_type(type) {
+    type.
+}
+
+
+
+// function setup_event_listeners_on_bag_types(bag_types) {
+//     bag_types.forEach((type) => {
+//         let types = document.querySelector("#filter-type_of_bag").children;
+//         console.log(types);
+//     });
+// }
+
+// setup_event_listeners_on_bag_types(container_types_of_bags);
+
+// document.querySelector("#button-exit").addEventListener("click", titleScreen);
+//
+//
+//
+//
+//Filipa kod
+
 let product_types = [];
 
 function clear_selected_types(product_types) {
@@ -16,6 +56,19 @@ function event_on_click_type(type) {
 function setup_event_listeners(product_types) {
     // for all types add event listener on click
 }
+
+
+// import (biblioteki albo inne pliki.js)
+
+// statatyczne zmienne (globalne)
+
+// definicje funkcji albo klasy
+
+// inne funkcje ktore cos rzeczywiscie robia
+// glowna funkcja ktora wywoluje inne funkcje
+
+// wykonanie
+
 
 setup_event_listeners(product_types);
 
@@ -35,3 +88,45 @@ function selectType() {}
 // what you click is being saved in local storage
 
 // when mobile make FILTER and SORT elements hidden when loading the page
+//
+//
+//
+//TO DO- 1. fetch it properly to restdb, 2. change properties inside template
+//Show each product
+//
+
+// function getdata() {
+//     fetch("https://kea21s-3790.restdb.io/rest/posts", {
+//         method: "GET",
+//         headers: {
+//             "x-apikey": "606d5f8cf553500431007514",
+//         },
+//     })
+//         .then((res) => res.json())
+//         .then((response) => {
+//             console.log(response);
+//             showPosts(response);
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//         });
+// }
+// getdata();
+
+// function showPosts(posts) {
+//     const template = document.querySelector("template.product-template")
+//         .content;
+
+//     posts.forEach((post) => {
+//         console.log(post);
+//         const copy = template.cloneNode(true);
+//         template.querySelector("h3").textContent = post.title;
+//         template.querySelector("h4 span").textContent = post.username;
+//         template.querySelector(
+//             "a.readmore"
+//         ).href = `article.html?articleId=${post._id}`;
+//         document.querySelector("div#product_list").appendChild(copy);
+//     });
+// }
+//
+//
