a0d21642d06bf2043c598a84f09463ce612d9022
Anna Thorslund <anna728x@stud.kea.dk>
2021-05-05 08:45:48 +0200

    menu dekstop doneeee

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function menuToggle() {
   var nav = document.getElementById("toggleIcon");
   nav.classList.toggle("active");
 }
+  /*SHOPMENU*/
+function shopMenuToggle() {
+  var nav = document.getElementById("shop-menu-overlay");
+  nav.classList.toggle("active");
+  var nav = document.getElementById("shopToggleIcon");
+  nav.classList.toggle("active");
+
+}
+
+  /*ABOUTMENU*/
+  function aboutMenuToggle() {
+    var nav = document.getElementById("about-menu-overlay");
+    nav.classList.toggle("active");
+    var nav = document.getElementById("aboutToggleIcon");
+    nav.classList.toggle("active");
+  
+  }
